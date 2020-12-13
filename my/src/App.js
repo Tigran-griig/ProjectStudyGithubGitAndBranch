a8e4ca68dad1c46ee7da57afc76392625e5e4674
Tigran-griig <tigrangrigoryan.it@gmail.com>
2020-12-13 16:17:47 +0400

    New React app

---
+++ my/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Gradient from "./Components";
+function App(props) {
+  console.log(props)
+  return (
+    <div className="App">
+  <h1>hello world</h1>
+      <Gradient />
+    </div>
+  );
+}
+
+export default App;
---
+++ my/src/Components/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Gradient extends React.Component{
+    render(props){
+
+        return (
+            <div araik={"vazgenchik"} className={"karxanyan"}>
+                araik
+            </div>
+        )
+    }
+}
+export default Gradient
